e06a5c7d9c2bd10178f59d7aa73102bcb8709ef5
David Veytsman <dveyts@yahoo.com>
2018-03-15 17:28:21 -0400

    tabs are functional

--- main.js
+++ main.js
@@ -8,10 +8,23 @@ window.onload = function(){
   for(var i = 0; i < tabLinks.length; i++){
     tabLinks[i].onclick = function(){
       displayPanel(this);
+      return false;
+    };
+    tabLinks[i].onfocus = function(){
+      displayPanel(this);
+      return false;
     };
   }
 function displayPanel(tabToActivate){
-
+  for(var j = 0; j < tabLinks.length; j++){
+    if(tabLinks[j] === tabToActivate){
+      tabLinks[j].classList.add("active");
+      tabPanels[j].style.display = 'block';
+    } else {
+      tabLinks[j].classList.remove("active");
+      tabPanels[j].style.display = 'none';
+    }
+  }
 }
 
 };
