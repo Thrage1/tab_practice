9fcf195e30f27f36d46933c6034a38a8add2e1bf
David Veytsman <dveyts@yahoo.com>
2018-03-15 17:21:41 -0400

    set display

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var tabPanels;
 window.onload = function(){
   tabLinks = document.getElementById('tabs').getElementsByTagName('li');
   tabPanels = document.getElementById('container').getElementsByTagName('div');
+  displayPanel(tabLinks[0]);
+  for(var i = 0; i < tabLinks.length; i++){
+    tabLinks[i].onclick = function(){
+      displayPanel(this);
+    };
+  }
+function displayPanel(tabToActivate){
 
+}
 
 };
