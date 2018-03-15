32012f9a5de7a9c9e789055d293b5a61da9c9267
David Veytsman <dveyts@yahoo.com>
2018-03-15 16:57:40 -0400

    grabbed the elements

---
+++ main.js
@@ -0,0 +1,9 @@
+var tabLinks;
+var tabPanels;
+
+window.onload = function(){
+  tabLinks = document.getElementById('tabs').getElementsByTagName('li');
+  tabPanels = document.getElementById('container').getElementsByTagName('div');
+
+
+};
